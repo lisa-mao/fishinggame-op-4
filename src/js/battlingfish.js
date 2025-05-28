cbcd8d27f0a4e91e4705c27b101b2760d799d7cf
lisa-mao <1091575@hr.nl>
2025-05-28 21:06:21 +0200

    change

---
+++ src/js/battlingfish.js
@@ -0,0 +1,35 @@
+import { Actor, Engine, Vector, DisplayMode, CollisionType, Keys, randomIntInRange, Random } from "excalibur"
+import { Resources, ResourceLoader } from './resources.js'
+
+export class BattlingFish extends Actor {
+    constructor() {
+        super({
+            width: Resources.BattlingFish.width,
+            height: Resources.BattlingFish.height
+        })
+        this.graphics.use(Resources.BattlingFish.toSprite())
+        this.pos = new Vector(30,0)
+        this.speed = 150
+        //  this.scale = new Vector(2, 2)
+
+        this.CollisionType = CollisionType.Passive
+
+        this.actions.repeatForever(builder => {
+
+
+            builder.moveBy(-60, 0, this.speed)
+            this.graphics.flipHorizontal = true     // flip de sprite
+
+            builder.moveBy(60, 0, this.speed)
+            this.graphics.flipHorizontal = false     // flip de sprite
+
+            builder.moveBy(-60, 0, this.speed)
+
+            builder.moveBy(60, 0, this.speed)
+
+        }
+        )
+    }
+
+
+}
---
+++ src/js/dobber.js
@@ -0,0 +1,62 @@
+import { Actor, Engine, Vector, DisplayMode, CollisionType, Keys, Animation, SpriteSheet, range, AnimationStrategy } from "excalibur"
+import { Resources, ResourceLoader } from './resources.js'
+import { Platform } from "./platform.js"
+import { Triggercircle } from "./trigger.js"
+import { ShadowFish } from "./fish.js"
+
+export class Dobber extends Actor {
+   
+    constructor(){
+         super({
+
+            width: Resources.Dobber.width,
+            height: Resources.Dobber.height,
+            
+            
+        })
+        
+        this.graphics.use(Resources.Dobber.toSprite())
+       
+    }
+
+    dobberActive(){
+        
+        this.pos = new Vector(700, 630)
+        this.scale = new Vector(3,3)
+        
+    }
+
+    dobberDeactive(){
+        
+        this.pos = new Vector(700, 830)
+        this.scale = new Vector(3,3)
+        
+    }
+
+    onInitialize(engine) {
+        this.on('collisionstart', (event) => this.hitSomething(event))
+        
+    }
+
+    hitSomething(event) {
+
+
+        if (event.other.owner instanceof ShadowFish) {
+            this.scene.engine.ui.triggerText("Press space when the fish aligns in the rectangle", 50, 650)
+
+            event.other.owner.resetposition()
+            // Je kan `instanceof` gebruiken om te zien waar je tegenaan botst.
+            
+            this.dobberDeactive()
+            this.scene.engine.battlingBar.barActive()
+            this.scene.engine.player.state = "fishreeling"
+            
+            // this.scene.engine.player.state = "battling"
+            
+            
+        }
+
+
+    }
+   
+}
