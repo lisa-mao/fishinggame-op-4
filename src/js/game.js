2ca1c705dbfb2526382d70d876ff74ed8fead3a7
lisa-mao <1091575@hr.nl>
2025-05-28 21:04:08 +0200

    game &resources

--- src/js/game.js
+++ src/js/game.js
@@ -1,32 +1,95 @@
 import '../css/style.css'
-import { Actor, Engine, Vector, DisplayMode } from "excalibur"
+import { Actor, Random, Engine, Vector, DisplayMode, SolverStrategy, CollisionType, Keys, Trigger } from "excalibur"
 import { Resources, ResourceLoader } from './resources.js'
+import { Elf } from './elf.js'
+import { Platform } from './platform.js'
+import { Background } from './background.js'
+import { ShadowFish } from './fish.js'
+import { Triggercircle } from './trigger.js'
+import { UI } from './ui.js'
+import { Dobber } from './dobber.js'
+import { BattlingBar } from './battlingbar.js'
+import { BattlingFish } from './battlingFish.js'
+import { Select } from './select.js'
+
+
+
 
 export class Game extends Engine {
 
+    player
+    ui
+
     constructor() {
-        super({ 
+        super({
             width: 1280,
             height: 720,
             maxFps: 60,
-            displayMode: DisplayMode.FitScreen
-         })
+            physics: {
+                solver: SolverStrategy.Arcade,
+                gravity: new Vector(0, 900),
+            },
+            displayMode: DisplayMode.FitScreen,
+        })
+        this.showDebug(true)
         this.start(ResourceLoader).then(() => this.startGame())
     }
 
+
+
     startGame() {
         console.log("start de game!")
-        const fish = new Actor()
-        fish.graphics.use(Resources.Fish.toSprite())
-        fish.pos = new Vector(500, 300)
-        fish.vel = new Vector(-10,0)
-        fish.events.on("exitviewport", (e) => this.fishLeft(e))
-        this.add(fish)
+        const mainB = new Background()
+        this.add(mainB)
+
+        const trigger = new Triggercircle()
+        this.add(trigger)
+
+        this.player = new Elf()
+        this.add(this.player)
+
+        this.dobbero = new Dobber()
+        this.add(this.dobbero)
+
+        const platform = new Platform()
+        this.add(platform)
+
+
+
+
+
+
+        let ypositions = [638, 620, 643, 675, 690, 700]
+        ypositions = ypositions.sort(() => Math.random() * 0.5)
+
+        for (let i = 0; i < ypositions.length; i++) {
+            let x = Math.random() + 1300
+            let speed = Math.random() * 125 + 50
+
+            const fish = new ShadowFish(x, ypositions[i], speed)
+            this.add(fish)
+        }
+
+
+        this.battlingBar = new BattlingBar()
+        this.add(this.battlingBar)
+
+        this.ui = new UI()
+        this.add(this.ui)
+
+
     }
 
-    fishLeft(e) {
-        e.target.pos = new Vector(1350, 300)
+    resetYOnHit() {
+        let ypositions = [638, 620, 643, 675, 690, 700]
+        ypositions = ypositions.sort(() => Math.random() * 0.5)
+
+        for (let i = 0; i < ypositions.length; i++) {
+
+            console.log(ypositions[i])
+        }
     }
+
 }
 
 new Game()
--- src/js/resources.js
+++ src/js/resources.js
@@ -1,11 +1,21 @@
 import { ImageSource, Sound, Resource, Loader } from 'excalibur'
+import { BattlingBar } from './battlingbar'
 
 // voeg hier jouw eigen resources toe
 const Resources = {
-    Fish: new ImageSource('images/fish.png')
+    MainBackground: new ImageSource('images/mainBackground.png'),
+    Elf: new ImageSource('images/elf.png'),
+    ElfSide: new ImageSource('images/elf-sideview.png'),
+    Platform: new ImageSource('images/platform2Background.png'),
+    Fish: new ImageSource('images/fishShadow.png'),
+    Trigger: new ImageSource('images/trigger.png'),
+    ElfFishing: new ImageSource('images/elf-move-spritesheet.png'),
+    Dobber: new ImageSource('images/dobber.png'),
+    BattlingBar: new ImageSource('images/battlingBar.png'),
+    BattlingFish: new ImageSource('images/battlingFish.png'),
+    Select: new ImageSource('images/select.png')
+    
 }
-
-
 
 
 const ResourceLoader = new Loader()
